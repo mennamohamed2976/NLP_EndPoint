b0ce449172336fb6f49cf726d5f3329ec7e98140
mennamohamed2976 <menna.8762004@gmail.com>
2026-06-14 21:02:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# src/config.py
 
 DATA_PATH = "C:/Users/Mona/OneDrive/Desktop/Medical_Report_Analysis2/Data/raw/nlp_medical_reports_1500_balanced.csv"
 PROCESSED_DATA_PATH = "C:/Users/Mona/OneDrive/Desktop/Medical_Report_Analysis2/Data/raw/processed_reports.csv"
